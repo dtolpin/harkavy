5ffe6a90c3f2c94675abdd55c614b3ff28fa195b
David Tolpin <david.tolpin@gmail.com>
2011-09-27 18:57:49 +0300

    nicer javascript

---
+++ yiddish.js
@@ -0,0 +1,35 @@
+/** Yiddish normalization routines */
+
+/* expand ligatures and remove diactritics */
+normal = function (word) {
+    return word.
+        replace(/װ/g, 'וו').
+        replace(/ױ/g, 'וי').
+        replace(/ײ/g, 'יי').
+        replace(/[ִַָּֿ]/g, '')
+}
+
+/* replace archaic prefixes and suffixes */
+variant = function (word) {
+    return word.
+        replace(/^הערו/, "ארו").
+        replace(/^ער/, "דער").
+        replace(/^פער/, "פאר").
+        replace(/^צו/, "צע").
+        replace(/ליך$/, "לעך").
+        replace(/ען$/, "ן")
+}
+
+/* generate a list of guesses from a list of words */
+guesses = function(words) {
+    var verter = []
+    words.each(function (word) {
+        word = normal(word)
+        verter.push(word)
+        var vort = variant(word)
+        if(vort!=word) {
+            verter.push(vort)
+        }
+    })
+    return verter
+}
